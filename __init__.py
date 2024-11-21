56176a42ef95099c2af65fdb135a174daf387b30
kobasrb <93743352+kobasrb@users.noreply.github.com>
2024-11-21 15:33:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,12 @@ def monhistogramme():
 @app.route("/commits/")
 def mescommits():
     return render_template("commits.html")
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
                                                                                                                                        
 @app.route('/')
 def hello_world():
